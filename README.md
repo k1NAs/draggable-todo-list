df8d01eec71a5e2f52dc099a5d0dc1707ed2da45
Andrii Korobkin <Flouzeek@gmail.com>
2023-01-21 08:40:18 +0200

    for deploy

---
+++ README.md
@@ -0,0 +1,3 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+This is To Do List App with draggable created items tasks. In this App was used react-draggable, uuid and randomcolor libraries. The tasks are creating in the same place and you can dragg the enywhere you want on th wievport. Tasks which are done you can esealy delete.  
---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { randomColor } from 'randomcolor';
+import Draggable from 'react-draggable';
+import './App.css';
+
+
+
+function App() {
+
+	const [task, setTask] = useState('');
+	const [tasks, setTasks] = useState(
+		JSON.parse(localStorage.getItem('tasks')) || []
+	);
+
+	useEffect(() => {
+		localStorage.setItem('tasks', JSON.stringify(tasks))
+	}, [tasks]);
+
+	const newTask = () => {
+		if (task.trim() !== '') {
+			const newTask = {
+				id: uuidv4(),
+				task: task,
+				color: randomColor({
+					luminosity: 'light'
+				}),
+				defaultPos: {
+					x: 1600,
+					y: -1000
+				}
+			}
+			setTasks((tasks) => [...tasks, newTask]);
+			setTask('')
+		} else {
+			alert('Enter your task please...');
+			setTask('');
+		}
+	}
+
+	const deleteNode = (id) => {
+		setTasks(tasks.filter((task) => task.id !== id));
+	}
+
+	const updatePos = (data, index) => {
+		let newArray = [...tasks];
+		newArray[index].defaultPos = { x: data.x, y: data.y };
+		setTasks(newArray);
+	}
+
+	const keyPress = (e) => {
+		const code = e.keyCode || e.which
+		if (code === 13) {
+			newTask()
+		}
+	}
+
+	return (
+   	<div className="App">
+			<div className='wrapper'>
+				<input
+				value={task}
+				type="text"
+				placeholder='Enter your task...'
+				onChange={ (e) => setTask(e.target.value) }
+				onKeyPress={(e) => keyPress(e)}
+			/>
+				<button className='btn__add' onClick={ newTask }>Enter</button>
+			</div>
+
+			{
+			tasks.map((task, index) =>{
+
+				return (
+					<Draggable 
+						key={index}
+						defaultPosition={task.defaultPos}
+						onStop={(_, data) =>{
+							updatePos(data, index);
+						}
+							
+						}
+					>
+
+						<div className='todo__task' style={{backgroundColor: task.color}}>
+							{(`${task.task}`)}
+							<button
+							className='btn__del'
+							onClick={() => deleteNode(task.id)}
+							>
+								X
+							</button>
+						</div>
+
+
+
+					</Draggable>
+				)
+			})
+		}
+
+   	</div>
+
+		
+	);
+}
+
+
+export default App;
